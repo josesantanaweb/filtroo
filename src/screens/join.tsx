de04e3514acbf4fd2462a4d75c4d141edec370ed
josesantana.web <josesantana.web@gmail.com>
2023-09-09 15:04:46 -0400

    feat: add animations

--- src/screens/join.tsx
+++ src/screens/join.tsx
@@ -23,10 +23,7 @@ import arrowsIcon from "../assets/arrows-icon.png";
 import QR from "../assets/onelinkto_cef3xk_svg.svg";
 import { set } from "store";
 import { isMobile } from "react-device-detect";
-
-import mobile1 from "../assets/3.svg";
-import mobile2 from "../assets/4.svg";
-import brandsLogos from "../assets/marcas.svg";
+import { motion } from "framer-motion";
 
 export default function Join() {
   const ref = useRef<any>();
@@ -116,47 +113,37 @@ export default function Join() {
       <Modal title="" ref={validationRef}>
         <h1>Please wait</h1>
       </Modal>
-      {/* <Modal title="" ref={ref}>
-        <div className="modal-success">
-          <CheckCircle
-            style={{
-              color: "#58c6ff",
-              marginBottom: 35,
-              transform: "scale(3)",
-            }}
-          />
-          <h2 style={{ textAlign: "center" }}>
-            Welcome to the club!<br></br>Please, verify your email.
-          </h2>
-          <p style={{ textAlign: "center" }}>
-            Now let’s get you some rewards for inviting your friends too.
-          </p>
-          <PrincipalButton
-            id="5"
-            onClick={() => {
-              navigate(`/`);
-            }}
-          >
-            Next
-          </PrincipalButton>
-        </div>
-      </Modal> */}
+
       <div className="index__container">
         <section className="index__container-section">
-          <div>
-            <h1
-              style={{ fontWeight: 700 }}
-              className="index__container-top-title"
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            exit={{ opacity: 0, transition: { duration: 1 } }}
+            variants={{ visible: { transition: { staggerChildren: 0.3 } } }}
+            className="index__container-section"
+          >
+            <motion.div
+              variants={{
+                hidden: { opacity: 0, y: -20 },
+                visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+              }}
+              style={{ top: "-18vw" } as any}
             >
-              You were invited to
-              <span style={{ fontWeight: 700 }}> Filtroo</span>
-            </h1>
+              <h1
+                style={{ fontWeight: 700 }}
+                className="index__container-top-title"
+              >
+                You were invited to
+                <span style={{ fontWeight: 700 }}> Filtroo</span>
+              </h1>
 
-            <p className="index__container-mid-subtitle">
-              Join, invite your friends, and start earning $1 or more for every
-              TikTok video you share!
-            </p>
-          </div>
+              <p className="index__container-mid-subtitle">
+                Join, invite your friends, and start earning $1 or more for
+                every TikTok video you share!
+              </p>
+            </motion.div>
+          </motion.div>
           {download && (
             <div className="index__container-QR">
               <p className="index__container-QR-title">
@@ -200,26 +187,6 @@ export default function Join() {
                 />
                 {state.code.length === 10 ? (
                   <>
-                    {/* <Input
-                    value={state.email}
-                    onChange={(e) => {
-                      setState((prev) => ({
-                        ...prev,
-                        email: e.target.value,
-                      }));
-                    }}
-                    placeholder="Email"
-                    />
-                    <Input
-                    value={state.username}
-                  onChange={(e) => {
-                    setState((prev) => ({
-                      ...prev,
-                      username: e.target.value,
-                    }));
-                  }}
-                  placeholder="TikTok username"
-                /> */}
                     <PhoneInput
                       placeholder="Phone number"
                       value={state.phone}
@@ -250,158 +217,138 @@ export default function Join() {
           </p> */}
         </section>
         <section className="index__container-section">
-          <h1 className="index__container-subtitle">
-            Here’s why 30+ million people recommend Filtroo
-          </h1>
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            exit={{ opacity: 0, transition: { duration: 1 } }}
+            variants={{ visible: { transition: { staggerChildren: 0.3 } } }}
+            className="index__container-section"
+          >
+            <motion.h1
+              variants={{
+                hidden: { opacity: 0, y: -20 },
+                visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+              }}
+              style={{ top: "-18vw" } as any}
+              className="index__container-subtitle"
+            >
+              Here’s why 30+ million people recommend Filtroo
+            </motion.h1>
+          </motion.div>
           <div className="index__items">
-            <div className="index__items-card">
-              <img src={flyIcon} alt="flyIcon" className="index__items-icon" />
-              <h6 className="index__items-title">Travel without hidden fees</h6>
-              <p className="index__items-text">
-                We don’t believe in financial borders, so it’s only fair that we
-                let you spend abroad in over 150 currencies with the{" "}
-              </p>
-            </div>
-            <div className="index__items-card">
-              <img
-                src={statisticsIcon}
-                alt="statisticsIcon"
-                className="index__items-icon"
-              />
-              <h6 className="index__items-title">
-                Stay on top of your expenses
-              </h6>
-              <p className="index__items-text">
-                We’ll show you exactly where your money is going each month, and
-                even let you set up monthly budgets for things like restaurants
-                and groceries.{" "}
-              </p>
-            </div>
-            <div className="index__items-card">
-              <img
-                src={vaultIcon}
-                alt="vaultIcon"
-                className="index__items-icon"
-              />
-              <h6 className="index__items-title">Save when you spend</h6>
-              <p className="index__items-text">
-                Round up every card payment to the nearest whole number, and
-                we'll stash your spare change away for you to put towards your
-                goals{" "}
-              </p>
-            </div>
-            <div className="index__items-card">
-              <img
-                src={arrowsIcon}
-                alt="arrowsIcon"
-                className="index__items-icon"
-              />
-              <h6 className="index__items-title">
-                Send & request money globally
-              </h6>
-              <p className="index__items-text">
-                Banks will charge you when you transfer money abroad. We're not
-                about that! Instantly send and request money from your friends
-                in Europe, U.S. and Australia at the touch of a button, and use
-                GIFs to make things a little more interesting.{" "}
-              </p>
-            </div>
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              exit={{ opacity: 0, transition: { duration: 1 } }}
+              variants={{ visible: { transition: { staggerChildren: 0.3 } } }}
+            >
+              <motion.div
+                variants={{
+                  hidden: { opacity: 0, x: -20 },
+                  visible: { opacity: 1, x: 0, transition: { duration: 0.5 } },
+                }}
+                className="index__items-card"
+              >
+                <img
+                  src={flyIcon}
+                  alt="flyIcon"
+                  className="index__items-icon"
+                />
+                <h6 className="index__items-title">
+                  Travel without hidden fees
+                </h6>
+                <p className="index__items-text">
+                  We don’t believe in financial borders, so it’s only fair that
+                  we let you spend abroad in over 150 currencies with the{" "}
+                </p>
+              </motion.div>
+            </motion.div>
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              exit={{ opacity: 0, transition: { duration: 1 } }}
+              variants={{ visible: { transition: { staggerChildren: 0.3 } } }}
+            >
+              <motion.div
+                variants={{
+                  hidden: { opacity: 0, x: 20 },
+                  visible: { opacity: 1, x: 0, transition: { duration: 0.5 } },
+                }}
+                className="index__items-card"
+              >
+                <img
+                  src={statisticsIcon}
+                  alt="statisticsIcon"
+                  className="index__items-icon"
+                />
+                <h6 className="index__items-title">
+                  Stay on top of your expenses
+                </h6>
+                <p className="index__items-text">
+                  We’ll show you exactly where your money is going each month,
+                  and even let you set up monthly budgets for things like
+                  restaurants and groceries.{" "}
+                </p>
+              </motion.div>
+            </motion.div>
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              exit={{ opacity: 0, transition: { duration: 1 } }}
+              variants={{ visible: { transition: { staggerChildren: 0.3 } } }}
+            >
+              <motion.div
+                variants={{
+                  hidden: { opacity: 0, x: -20 },
+                  visible: { opacity: 1, x: 0, transition: { duration: 0.5 } },
+                }}
+                className="index__items-card"
+              >
+                <img
+                  src={vaultIcon}
+                  alt="vaultIcon"
+                  className="index__items-icon"
+                />
+                <h6 className="index__items-title">Save when you spend</h6>
+                <p className="index__items-text">
+                  Round up every card payment to the nearest whole number, and
+                  we'll stash your spare change away for you to put towards your
+                  goals{" "}
+                </p>
+              </motion.div>
+            </motion.div>
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              exit={{ opacity: 0, transition: { duration: 1 } }}
+              variants={{ visible: { transition: { staggerChildren: 0.3 } } }}
+            >
+              <motion.div
+                variants={{
+                  hidden: { opacity: 0, x: 20 },
+                  visible: { opacity: 1, x: 0, transition: { duration: 0.5 } },
+                }}
+                className="index__items-card"
+              >
+                <img
+                  src={arrowsIcon}
+                  alt="arrowsIcon"
+                  className="index__items-icon"
+                />
+                <h6 className="index__items-title">
+                  Send & request money globally
+                </h6>
+                <p className="index__items-text">
+                  Banks will charge you when you transfer money abroad. We're
+                  not about that! Instantly send and request money from your
+                  friends in Europe, U.S. and Australia at the touch of a
+                  button, and use GIFs to make things a little more interesting.{" "}
+                </p>
+              </motion.div>
+            </motion.div>
           </div>
         </section>
       </div>
-      {/* <div className="index__container index__container-middle">
-        <section>
-          <div>
-            <h1 style={{ fontWeight: 700 }} className="index__container-top">
-              Join the Filtroo Referral Program and earn up to $100 in Rewards!
-            </h1>
-
-            <p className="index__container-bottom">
-              Register Now to Start Inviting Friends and Earning Points.
-            </p>
-
-            <p className="index__container-mid">
-              Unlock exclusive rewards by participating in the Filtroo Referral
-              Program. Share your unique referral link with your friends and
-              earn points for every friend who signs up. Now redeem your points
-              for exciting rewards!
-            </p>
-          </div>
-        </section>
-        <section>
-          <img
-            src={mobile1}
-            style={{ width: "400px", height: "520px" }}
-            alt="movile interface 1"
-          />
-        </section>
-      </div>
-      <div className="index__container index__container-desktop">
-        <section>
-          <img
-            src={mobile2}
-            style={{ width: "400px", height: "520px" }}
-            alt="mobile interface 2"
-          />
-        </section>
-        <section>
-          <div>
-            <h1 style={{ fontWeight: 700 }} className="index__container-top">
-              Invite Your Friends and Earn Rewards Together!
-            </h1>
-
-            <p className="index__container-bottom">
-              Share your exclusive referral link and both you and your friend
-              will earn 100 points (10$) when they register.
-            </p>
-
-            <p className="index__container-mid">
-              • Earn Rewards Each Time You Share Content on TikTok! <br />
-              • Explore Endless Creative Filters! <br />
-              • Invite Friends & Earn More! <br />
-              • Join the Most Creative Community! <br />
-            </p>
-            <img
-              src={brandsLogos}
-              style={{ width: "100%", height: "30%" }}
-              alt="brands"
-            />
-          </div>
-        </section>
-      </div>
-      <div className="index__container index__container-mobile">
-        <section>
-          <div>
-            <h1 style={{ fontWeight: 700 }} className="index__container-top">
-              Invite Your Friends and Earn Rewards Together!
-            </h1>
-
-            <p className="index__container-bottom">
-              Share your exclusive referral link and both you and your friend
-              will earn 100 points (10$) when they register.
-            </p>
-
-            <p className="index__container-mid">
-              • Earn Rewards Each Time You Share Content on TikTok! <br />
-              • Explore Endless Creative Filters! <br />
-              • Invite Friends & Earn More! <br />
-              • Join the Most Creative Community! <br />
-            </p>
-            <img
-              src={brandsLogos}
-              style={{ width: "100%", height: "30%" }}
-              alt="brands"
-            />
-          </div>
-        </section>
-        <section>
-          <img
-            src={mobile2}
-            style={{ width: "400px", height: "520px" }}
-            alt="mobile interface 2"
-          />
-        </section>
-      </div> */}
     </>
   );
 }
